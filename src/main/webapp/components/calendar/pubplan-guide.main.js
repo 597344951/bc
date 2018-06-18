06517ec7a32800af455ae6e08e8b0c5f33ed352c
Wangch <597344951@qq.com>
2018-06-18 11:17:08 +0800

    发布计划向导界面

---
+++ src/main/webapp/components/calendar/pubplan-guide.main.js
@@ -0,0 +1,32 @@
+/** 活动推荐组件 **/
+
+let PubplanGuide = {
+    info: {
+        name: 'pubplan-guide',
+        template_url: '/components/calendar/pubplan-guide.view.html',
+        author: 'Wangch',
+        descript: '发布计划向导'
+    },
+    props: {
+        visiable: {
+            type: Boolean,
+            default: true
+        }
+    },
+    data: function () {
+        return {
+             
+        }
+    },
+    watch: {
+         
+    },
+    mounted() {
+    },
+    computed: {},
+    methods: {},
+    //模版内容
+    template: ''
+}
+
+export default PubplanGuide;
